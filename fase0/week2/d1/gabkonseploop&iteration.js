6bbba10c7e62d2cee56b341e328fae7a67533111
ppsari <poppymighty@gmail.com>
2017-03-21 11:41:44 +0700

    w2d1 tambahin quotation

--- fase0/week2/d1/gabkonseploop&iteration.js
+++ fase0/week2/d1/gabkonseploop&iteration.js
@@ -1,11 +1,15 @@
 for(var i = 1; i <= 100; i++)
 	if( i%2 == 0 ) console.log(i+" "+"GENAP");
 	else console.log(i+" "+"GANJIL");
-
-
+	
 for(var i=1; i<=100; i+=2)
-	if(i%3 == 0) console.log(i+" KELIPATAN 3" );
+  if(i%3 === 0) console.log(i+" KELIPATAN 3" );
+	else console.log('\"\"');
+	
 for(var i=1; i<=100; i+=5)
-	if(i%6  == 0) console.log(i+" KELIPATAN 6";
+	if(i%6  == 0) console.log(i+" KELIPATAN 6");
+	else console.log('\"\"');
+	
 for(var i=1; i<=100; i+=9)
 	if(i%10  == 0) console.log(i+" KELIPATAN 10");
+	else console.log('\"\"');
